3918b53afb1f09ee3aaf23a81459329edf5eb1dd
cnalzr <cnalzr@gmail.com>
2022-05-26 23:21:06 +0800

    2022年5月26日02点13分

--- src/main/java/com/lzr/dao/LoginAuthDao.java
+++ src/main/java/com/lzr/dao/LoginAuthDao.java
@@ -1,13 +1,10 @@
 package com.lzr.dao;
-
 import com.lzr.model.RbacManager;
 import org.apache.ibatis.annotations.Param;
-
 import java.util.Set;
-
 public interface LoginAuthDao {
     RbacManager login(@Param("account") String account,@Param("password") String password);
     RbacManager getByName(String username);
-    Set<String> getRoles(String username);
-    Set<String> getPermsByName(String username);
+    Set<String> getRoles(String account);
+    Set<String> getPermsByName(String account);
 }
